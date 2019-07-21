56a231bd4dcdc3ffc347492e250e88b2855cf3e8
kinironote <ishowta@gmail.com>
2019-07-21 16:23:38 +0900

    [clean] Modelのドキュメントを追加

--- Sources/Bot/Models/model.swift
+++ Sources/Bot/Models/model.swift
@@ -11,26 +11,32 @@ extension Expression where Datatype == Date {
         return Expression<String>("strftime('\(format)', \(template))")
     }
 
+    /// Get year
     var y: Expression<String> {
         return Expression<String>("strftime('%y', \(template))")
     }
 
+    /// Get month
     var m: Expression<String> {
         return Expression<String>("strftime('%m', \(template))")
     }
 
+    /// Get day
     var d: Expression<String> {
         return Expression<String>("strftime('%d', \(template))")
     }
 
+    /// Get hour
     var h: Expression<String> {
         return Expression<String>("strftime('%h', \(template))")
     }
 
+    /// Get minute
     var M: Expression<String> {
         return Expression<String>("strftime('%M', \(template))")
     }
 
+    /// Get second
     var s: Expression<String> {
         return Expression<String>("strftime('%s', \(template))")
     }
